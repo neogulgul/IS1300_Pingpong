f024658c31fac720bf27e81b9a76266dea2138f4
neo <neogullberg@tutanota.com>
2025-11-03 00:35:21 +0100

    prototype

--- include/pingpong.h
+++ include/pingpong.h
@@ -5,10 +5,11 @@
 
 typedef enum State
 {
+	STATE_UNDEFINED,
 	STATE_START,
 	STATE_MOVE_LEFT,
 	STATE_MOVE_RIGHT,
-	STATE_SHOW_SCORE
+	STATE_SCORE_DISPLAY
 } State;
 
 typedef struct Press
@@ -24,17 +25,28 @@ typedef struct Pingpong
 	State state, next_state;
 	Press l_press, r_press;
 	uint8_t l_score, r_score;
+	bool left_player_served;
+	uint8_t ball_position;
+	uint16_t ball_delay_ms;
 } Pingpong;
 
 void sleep(uint32_t ms);
 
 void led_set_state(uint8_t led_index, bool state);
+void led_set_state_all(bool state);
 
 void Press_update(Press *this, uint8_t pressed_this_update);
 
-void Pingpong_init             (Pingpong *this);
-void Pingpong_update           (Pingpong *this);
-void Pingpong_handle_start     (Pingpong *this);
-void Pingpong_handle_move_left (Pingpong *this);
-void Pingpong_handle_move_right(Pingpong *this);
-void Pingpong_handle_show_score(Pingpong *this);
+void Pingpong_init                (Pingpong *this);
+void Pingpong_reset               (Pingpong *this);
+void Pingpong_ball_speed_reset    (Pingpong *this);
+void Pingpong_ball_speed_increase (Pingpong *this);
+void Pingpong_update              (Pingpong *this);
+void Pingpong_serve_from_the_left (Pingpong *this);
+void Pingpong_serve_from_the_right(Pingpong *this);
+void Pingpong_handle_start        (Pingpong *this);
+void Pingpong_handle_move_left    (Pingpong *this);
+void Pingpong_handle_move_right   (Pingpong *this);
+void Pingpong_handle_score_display(Pingpong *this);
+void Pingpong_draw_ball           (Pingpong *this);
+bool Pingpong_has_a_winner        (Pingpong *this);
--- src/pingpong.c
+++ src/pingpong.c
@@ -1,11 +1,26 @@
+#include <stdio.h>
+
 #include "gpio.h"
 
 #include "buttons.h"
 #include "pingpong.h"
 
-void sleep(uint32_t ms)
+#define BALL_DELAY_MS_BASE      200
+#define BALL_DELAY_MS_DECREMENT  20
+#define SCORE_DISPLAY_PAUSE_MS 3000
+
+bool interrupt_occurred_whilst_waiting(bool *interrupt, uint32_t ms, bool break_on_interrupt)
 {
-	HAL_Delay(ms);
+	uint32_t start = HAL_GetTick();
+	*interrupt = false;
+	for (;;)
+	{
+		uint32_t elapsed = HAL_GetTick() - start;
+		bool timed_out = elapsed >= ms;
+		bool interrupted = *interrupt && break_on_interrupt;
+		if (timed_out || interrupted) break;
+	}
+	return *interrupt;
 }
 
 void led_set_state(uint8_t led_index, bool state)
@@ -26,6 +41,14 @@ void led_set_state(uint8_t led_index, bool state)
 	}
 }
 
+void led_set_state_all(bool state)
+{
+	for (uint8_t i = 0; i < 8; i++)
+	{
+		led_set_state(i, state);
+	}
+}
+
 void Press_update(Press *this, uint8_t pressed_this_update)
 {
 	this->pressed_last_update = this->pressed_this_update;
@@ -37,61 +60,206 @@ void Press_update(Press *this, uint8_t pressed_this_update)
 
 void Pingpong_init(Pingpong *this)
 {
-	this->state = STATE_START;
-	this->next_state = this->state;
-	this->l_score = 0;
-	this->r_score = 0;
+	this->state      = STATE_UNDEFINED;
+	this->next_state = STATE_START;
 }
 
-bool interrupt_occurred_whilst_sleeping(bool *occurred, uint32_t ms)
+void Pingpong_reset(Pingpong *this)
 {
-	*occurred = false;
-	HAL_Delay(ms);
-	return *occurred;
+	this->l_score = 0;
+	this->r_score = 0;
+	Pingpong_ball_speed_reset(this);
+}
+
+void Pingpong_ball_speed_reset(Pingpong *this)
+{
+	this->ball_delay_ms = BALL_DELAY_MS_BASE;
+}
+
+void Pingpong_ball_speed_increase(Pingpong *this)
+{
+	if (this->ball_delay_ms > BALL_DELAY_MS_DECREMENT)
+	{
+		this->ball_delay_ms -= BALL_DELAY_MS_DECREMENT;
+	}
 }
 
 void Pingpong_update(Pingpong *this)
 {
-	this->state = this->next_state;
-
 	bool l_pressed_this_update = HAL_GPIO_ReadPin(L_button_GPIO_Port, L_button_Pin) == 0;
 	bool r_pressed_this_update = HAL_GPIO_ReadPin(R_button_GPIO_Port, R_button_Pin) == 0;
 	Press_update(&this->l_press, l_pressed_this_update);
 	Press_update(&this->r_press, r_pressed_this_update);
 
-	static uint8_t led = 0;
+	State old_state = this->state;
+	this->state = this->next_state;
 
-	if (this->l_press.just_pressed) led = (led == 0) ? 7 : led - 1;
-	if (this->r_press.just_pressed) led = (led == 7) ? 0 : led + 1;
+	bool state_changed = this->state != old_state;
 
-	// if (interrupt_occurred_whilst_sleeping(&l_button_pressed, 100))
-	// {
-	// 	led = (led == 7) ? 0 : led + 1;
-	// }
+	if (state_changed)
+	{
+		switch (this->state)
+		{
+			case STATE_START:
+			{
+				Pingpong_reset(this);
+				printf("Start by pressing on of the buttons.\n");
+				break;
+			}
+			case STATE_MOVE_LEFT:
+			{
+				printf("Ball now moves left.\n");
+				break;
+			}
+			case STATE_MOVE_RIGHT:
+			{
+				printf("Ball now moves right.\n");
+				break;
+			}
+			case STATE_SCORE_DISPLAY:
+			{
+				printf("(%i:%i) Left player has %i points. Right player has %i points.\n", this->l_score, this->r_score, this->l_score, this->r_score);
+				break;
+			}
+		}
+	}
 
-	for (uint8_t i = 0; i < 8; i++) led_set_state(i, i == led);
+	switch (this->state)
+	{
+		case STATE_START        : { Pingpong_handle_start        (this); break; }
+		case STATE_MOVE_LEFT    : { Pingpong_handle_move_left    (this); break; }
+		case STATE_MOVE_RIGHT   : { Pingpong_handle_move_right   (this); break; }
+		case STATE_SCORE_DISPLAY: { Pingpong_handle_score_display(this); break; }
+	}
+}
 
-	// switch (this->state)
-	// {
-	// 	case STATE_START     : { Pingpong_handle_start     (this); break; }
-	// 	case STATE_MOVE_LEFT : { Pingpong_handle_move_left (this); break; }
-	// 	case STATE_MOVE_RIGHT: { Pingpong_handle_move_right(this); break; }
-	// 	case STATE_SHOW_SCORE: { Pingpong_handle_show_score(this); break; }
-	// }
+void Pingpong_serve_from_the_left(Pingpong *this)
+{
+	this->next_state         = STATE_MOVE_RIGHT;
+	this->ball_position      = 0;
+	this->left_player_served = true;
+}
+
+void Pingpong_serve_from_the_right(Pingpong *this)
+{
+	this->next_state         = STATE_MOVE_LEFT;
+	this->ball_position      = 7;
+	this->left_player_served = false;
 }
 
 void Pingpong_handle_start(Pingpong *this)
 {
+	bool both_pressed = this->l_press.just_pressed && this->r_press.just_pressed;
+	if (both_pressed) return;
+
+	if (this->l_press.just_pressed)
+	{
+		Pingpong_serve_from_the_left(this);
+	}
+	else if (this->r_press.just_pressed)
+	{
+		Pingpong_serve_from_the_right(this);
+	}
+
+	led_set_state_all(0);
 }
 
 void Pingpong_handle_move_left(Pingpong *this)
 {
+	Pingpong_draw_ball(this);
+	bool at_edge = this->ball_position == 0;
+	if (interrupt_occurred_whilst_waiting(&l_button_pressed, this->ball_delay_ms, at_edge))
+	{
+		if (at_edge)
+		{
+			this->next_state = STATE_MOVE_RIGHT;
+			this->ball_position = 1;
+			Pingpong_ball_speed_increase(this);
+			return;
+		}
+	}
+	else
+	{
+		if (at_edge)
+		{
+			this->r_score++;
+			this->next_state = STATE_SCORE_DISPLAY;
+		}
+	}
+	this->ball_position--;
 }
 
 void Pingpong_handle_move_right(Pingpong *this)
 {
+	Pingpong_draw_ball(this);
+	bool at_edge = this->ball_position == 7;
+	if (interrupt_occurred_whilst_waiting(&r_button_pressed, this->ball_delay_ms, at_edge))
+	{
+		if (at_edge)
+		{
+			this->next_state = STATE_MOVE_LEFT;
+			this->ball_position = 6;
+			Pingpong_ball_speed_increase(this);
+			return;
+		}
+	}
+	else
+	{
+		if (at_edge)
+		{
+			this->l_score++;
+			this->next_state = STATE_SCORE_DISPLAY;
+		}
+	}
+	this->ball_position++;
 }
 
-void Pingpong_handle_show_score(Pingpong *this)
+void Pingpong_handle_score_display(Pingpong *this)
 {
+	Pingpong_ball_speed_reset(this);
+
+	switch (this->l_score)
+	{
+		case 0: { led_set_state(0, 0); led_set_state(1, 0); led_set_state(2, 0); led_set_state(3, 0); break; }
+		case 1: { led_set_state(0, 1); led_set_state(1, 0); led_set_state(2, 0); led_set_state(3, 0); break; }
+		case 2: { led_set_state(0, 1); led_set_state(1, 1); led_set_state(2, 0); led_set_state(3, 0); break; }
+		case 3: { led_set_state(0, 1); led_set_state(1, 1); led_set_state(2, 1); led_set_state(3, 0); break; }
+		case 4: { led_set_state(0, 1); led_set_state(1, 1); led_set_state(2, 1); led_set_state(3, 1); break; }
+	}
+	switch (this->r_score)
+	{
+		case 0: { led_set_state(7, 0); led_set_state(6, 0); led_set_state(5, 0); led_set_state(4, 0); break; }
+		case 1: { led_set_state(7, 1); led_set_state(6, 0); led_set_state(5, 0); led_set_state(4, 0); break; }
+		case 2: { led_set_state(7, 1); led_set_state(6, 1); led_set_state(5, 0); led_set_state(4, 0); break; }
+		case 3: { led_set_state(7, 1); led_set_state(6, 1); led_set_state(5, 1); led_set_state(4, 0); break; }
+		case 4: { led_set_state(7, 1); led_set_state(6, 1); led_set_state(5, 1); led_set_state(4, 1); break; }
+	}
+
+	HAL_Delay(SCORE_DISPLAY_PAUSE_MS);
+
+	if (Pingpong_has_a_winner(this))
+	{
+		this->next_state = STATE_START;
+		return;
+	}
+
+	if (this->left_player_served)
+	{
+		Pingpong_serve_from_the_right(this);
+	}
+	else
+	{
+		Pingpong_serve_from_the_left(this);
+	}
+}
+
+void Pingpong_draw_ball(Pingpong *this)
+{
+	led_set_state_all(0);
+	led_set_state(this->ball_position, 1);
+}
+
+bool Pingpong_has_a_winner(Pingpong *this)
+{
+	return this->l_score == 4 || this->r_score == 4;
 }
